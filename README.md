1f70ac008246aea22c201a27fe30aaba3adecd38
HarshwardhanSingh <harsh.rathore14@gmail.com>
2017-12-29 19:10:54 +0530

    change the config keys and update readme

--- README.md
+++ README.md
@@ -20,12 +20,15 @@ const WebpackBundleSizeCheckPlugin = require('webpack-bundle-size-check-plugin')
 plugins: [
   new WebpackBundleSizeCheckPlugin([
     {
-      name: 'app.bundle.js',  // exact name of output file
-      size: '100'     // in KB
+      chunkName: 'app.bundle.js',  // exact name of output file
+      maxSize: '100',     // in KB
+      mode: 'strict    // "non-strict" by default
     }
   ])
 ]
 ```
 and that's it.
+
+##### If you select "strict" mode then the build will fail if the size exceeds the maxSize specified. If you select "non-strict" (default) mode then you will get a warning if the size exceeds the specified limit.
 
 Now if you build size exceeds the allowed size, your build will fail with proper error message like `Build Size for app.bundle.js exceeded the permitted size 100KB`
--- index.js
+++ index.js
@@ -21,19 +21,19 @@ class WebpackBundleSizeCheckPlugin {
       const { assets } = stats.compilation;
       const assetsName = Object.keys(assets);
       this.options.forEach(assetObj => {
-        if (assetsName.indexOf(assetObj.name) > -1) {
-          const size = (assets[assetObj.name]['_cachedSize']/1024);
-          if (size > Number(assetObj.size)) {
-            const diffInSize = (size - Number(assetObj.size)).toFixed(2);
-            const message = `Build Size for ${assetObj.name} exceeded the permitted size ${assetObj.size} KB by ${diffInSize} KB`;
+        if (assetsName.indexOf(assetObj.chunkName) > -1) {
+          const size = (assets[assetObj.chunkName]['_cachedSize']/1024);
+          if (size > Number(assetObj.maxSize)) {
+            const diffInSize = (size - Number(assetObj.maxSize)).toFixed(2);
+            const message = `Build Size for ${assetObj.chunkName} exceeded the permitted size. Current size: ${size.toFixed(2)} KB but the allowed maxSize is ${assetObj.maxSize} KB`;
 
             if (assetObj.mode.toLowerCase() === MODE.STRICT.toLowerCase()) {
               throw new Error(chalk.white.bgRed.bold(message));
             }
 
             console.error(chalk.yellow.bold('Warning: ' + message));
-          } else if ((Number(assetObj.size) - size) <= warningSize) {
-            console.error(chalk.yellow.bold(`Warning: Bundle size for ${assetObj.name} is reaching towards the specified limit`));
+          } else if ((Number(assetObj.maxSize) - size) <= warningSize) {
+            console.error(chalk.yellow.bold(`Warning: Bundle size for ${assetObj.chunkName} is reaching towards the maxSize specified. Time to take some string measures`));
           }
         }
       });
