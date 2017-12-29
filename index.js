f026a596ad966f73ea166fa43784d8aba074f427
HarshwardhanSingh <harsh.rathore14@gmail.com>
2017-12-29 18:49:20 +0530

    allow mode option for more control

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+"use strict"
+
 const chalk = require('chalk');
+
+const MODE = {
+  'STRICT': 'strict',
+  'NON-STRICT': 'non-strict'
+};
+const warningSize = 50;
+
 class WebpackBundleSizeCheckPlugin {
   constructor(options) {
     this.options = options;
@@ -15,7 +24,16 @@ class WebpackBundleSizeCheckPlugin {
         if (assetsName.indexOf(assetObj.name) > -1) {
           const size = (assets[assetObj.name]['_cachedSize']/1024);
           if (size > Number(assetObj.size)) {
-            throw new Error(chalk.red(`Build Size for ${assetObj.name} exceeded the permitted size ${assetObj.size}KB`));
+            const diffInSize = (size - Number(assetObj.size)).toFixed(2);
+            const message = `Build Size for ${assetObj.name} exceeded the permitted size ${assetObj.size} KB by ${diffInSize} KB`;
+
+            if (assetObj.mode.toLowerCase() === MODE.STRICT.toLowerCase()) {
+              throw new Error(chalk.white.bgRed.bold(message));
+            }
+
+            console.error(chalk.yellow.bold('Warning: ' + message));
+          } else if ((Number(assetObj.size) - size) <= warningSize) {
+            console.error(chalk.yellow.bold(`Warning: Bundle size for ${assetObj.name} is reaching towards the specified limit`));
           }
         }
       });
